f7fccdfc3e82951c160e44ad65ba54763dba05ac
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-06 21:11:26 -0500

    should have content centered now

--- docs/script.js
+++ docs/script.js
@@ -5,7 +5,7 @@ var cityResult = document.getElementById("result");
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={APIkey}
 console.log(city.value);
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + APIKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 //a function that when called will run a query on weather API
 function fetchWeather1() {
   fetch(queryURL)
@@ -15,22 +15,22 @@ function fetchWeather1() {
 .then(function (data) {
   console.log("hello, below you'll find the url");
   console.log(data);
-  for (var i = 0; i <= data.length; i++) 
-  {
-    console.log(data[i]);
-  var cityResult = document.createElement("p");
-				cityResult.textContent = (data[i]);
-        result.append(cityResult);
-      }
+  // for (var i = 0; i <= data.length; i++) 
+  // {
+  //   console.log(data[i]);
+  // var cityResult = document.createElement("p");
+	// 			cityResult.textContent = (data[i]);
+  //       result.append(cityResult);
+  //     }
 });
 }
 //Added a search Button to run the function on click
-document.getElementById("searchButton").addEventListener("click", function (event) {
+document.getElementById("searchForm").addEventListener("submit", function (event) {
   event.preventDefault();
-	console.log("you typed something in");
+	console.log("you submitted on the form");
 	fetchWeather1();
 });
-fetchWeather1();
+
 
 //write an event listener and a query selector (the event listener will have a function inside of it) set a variable listening to that whole form
 // addEventListener("submit",function(){}
