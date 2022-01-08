b7e0f305f1e7bf5fbc68af55ba46c3dec4b8be49
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-08 01:07:17 -0500

    working on second function to get daily stuff

--- docs/script.js
+++ docs/script.js
@@ -13,38 +13,50 @@ var dayToday = moment().format("dddd, MMMM Do YYYY");
 function fetchWeather1() {
 	var city = document.getElementById("city").value;
 	var queryURL =
-		"https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial" +
+		"https://api.openweathermap.org/data/2.5/weather?q=" +
+		city +
+		"&units=imperial" +
 		"&appid=" +
 		APIKey;
 	fetch(queryURL)
 		.then(function (response) {
-      if (response.status === 404){console.log("404 problem");
-      var queryError = document.createElement("p");
-      queryError.textContent="NO CITY FOUND BY THAT NAME, PLEASE TRY AGAIN.";
-      currentWeatherResult.append(queryError)
-    }
-    else if (city === ""){var queryError = document.createElement("p");
-    queryError.textContent="NO CITY FOUND BY THAT NAME, PLEASE TRY AGAIN.";
-    currentWeatherResult.append(queryError)}
+			if (response.status === 404) {
+				console.log("404 problem");
+				var queryError = document.createElement("p");
+				queryError.textContent =
+					"NO CITY FOUND BY THAT NAME, PLEASE TRY AGAIN.";
+				currentWeatherResult.append(queryError);
+			} else if (city === "") {
+				var queryError = document.createElement("p");
+				queryError.textContent =
+					"NO CITY FOUND BY THAT NAME, PLEASE TRY AGAIN.";
+				currentWeatherResult.append(queryError);
+			}
 			return response.json();
 		})
 		.then(function (data) {
-		var queryResult = document.createElement("textarea");
-      //TODO: make the weather icons work - https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2 
-      iconImage = document.createElement("img");
-      nameData = data.name;
+			var queryResult = document.createElement("textarea");
+			//TODO: make the weather icons work - https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+			iconImage = document.createElement("img");
+			nameData = data.name;
 			dateData = dayToday;
 			conditionIconData =
 				data.weather[0].main +
 				", " +
 				data.weather[0].description +
 				" " +
-				data.weather[0].icon + iconImage;
+				data.weather[0].icon +
+				iconImage;
 			temperatureData = data.main.temp;
 			humidityData = data.main.humidity;
 			windSpeedData =
-				"\n  Speed: " + data.wind.speed + "\n  Deg: " + data.wind.deg + "\n  Gust: " + data.wind.gust;
-      //TODO: figure out how to find and calculate/display UV information  
+				"\n  Speed: " +
+				data.wind.speed +
+				"\n  Deg: " +
+				data.wind.deg +
+				"\n  Gust: " +
+				data.wind.gust;
+			//TODO: figure out how to find and calculate/display UV information
 			uvData = "UV";
 			queryResult.setAttribute("class", "weatherCurrent");
 			queryResult.textContent =
@@ -69,7 +81,6 @@ function fetchWeather1() {
 				"UV: " +
 				uvData;
 			currentWeatherResult.append(queryResult);
-			console.log(queryResult);
 			// for (var i = 0; i <= data.length; i++) {
 			// 	console.log(data[i]);
 			// 	var cityResult = document.createElement("p");
@@ -78,32 +89,32 @@ function fetchWeather1() {
 		});
 }
 
-
 function fetchWeather2() {
 	var city = document.getElementById("city").value;
+  var geoLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + city +"&limit=3&appid=" + APIKey;
+  console.log(geoLocation);
 	var queryURL5 =
-		"http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city +"&cnt=5" +
-		"&appid=" +
-		APIKey2;
-	fetch(queryURL5)
-  		.then(function (response) {
-      console.log("working on 5 day forecast function and also mary is awesome");
-      return response.json();
+  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily" +	"&appid=" + APIKey2;
+
+		fetch(queryURL5)
+		.then(function (response) {
+			console.log(
+				"working on 5 day forecast function and also mary is awesome"
+			);
+			return response.json();
 		})
 		.then(function (data) {
 			console.log(data);
-    });
-  }
+		});
+}
 
 //Added a search Button to run the function on click
-document
-	.getElementById("search")
-	.addEventListener("click", function (event) {
-		event.preventDefault();
-		console.log("you submitted on the form");
-		fetchWeather1();
-    fetchWeather2();
-	});
+document.getElementById("search").addEventListener("click", function (event) {
+	event.preventDefault();
+	console.log("you submitted on the form");
+	fetchWeather1();
+	fetchWeather2();
+});
 
 //write an event listener and a query selector (the event listener will have a function inside of it) set a variable listening to that whole form
 // addEventListener("submit",function(){}
